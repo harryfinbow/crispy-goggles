a5a65a28d34c7dedeeaa3869419f06f0d286f379
Harry Finbow <harryfinbow@gmail.com>
2022-04-08 15:36:11 +0100

    Adds basic scoring system

--- goggles.py
+++ goggles.py
@@ -4,10 +4,11 @@ import time
 import pyshark
 import pandas
 
-SkewLimit = 0.25
-PeriodicityJitterThreshold = 25
-LengthJitterThreshold = 5
-DurationJitterThreshold = 100
+SkewLimit = 0.1
+PeriodicitySpreadThreshold = 0.05
+PeriodicityJitterThreshold = 30
+LengthSpreadThreshold = 0.05
+LengthJitterThreshold = 10
 BeaconIntervalThreshold = 900
 MinStreamCount = 10
 
@@ -18,16 +19,17 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
 
-def print_every(x, obj, obj2=0):
+def print_every(x, stats, flagged):
     if not print_every.counter % x:
         current_time = time.time()
         cls()
-        print('Packets Analysed:', print_every.counter)
-        print("Time Elapsed:     %.2f seconds" % (current_time - start_time))
-        print("Time per 1000:    %.2f seconds" % (current_time - print_every.last_time))
-        print(obj2)
-        print(obj)
-        print(obj.loc[obj['Malicious'] == 1])
+        print('Packets Analysed:  ', print_every.counter)
+        print("Time Elapsed:\t    %.2f seconds" % (current_time - start_time))
+        print("Time per %s:\t    %.2f seconds" % (x, (current_time - print_every.last_time)))
+        print("Packets per second: %s" % (int(x / (current_time - print_every.last_time))))
+        print(stats)
+        print(stats.loc[stats['Malicious'] == 1])
+        print(flagged)
 
         print_every.last_time = current_time
 
@@ -38,19 +40,25 @@ print_every.last_time = 0
 
 def capture_live_packets(network_interface):
     tcp_streams = {}
-    tcp_streams_statistics = pandas.DataFrame(columns = ['Stream count', 'Periodicity jitter', 'Periodicity skew', 'Duration jitter', 'Duration skew', 'Length jitter', 'Length skew'])
+    tcp_streams_statistics = pandas.DataFrame(columns = ['Stream count', ('Periodicity', 'Spread'), ('Periodicity', 'Jitter'), ('Periodicity', 'Skew'), ('Length', 'Spread'), ('Length', 'Jitter'), ('Length', 'Skew'), 'Malicious'])
+    flagged_tcp_streams = []
 
     capture = pyshark.LiveCapture(interface=network_interface, only_summaries = True)
     
-    for raw_packet in capture.sniff_continuously():
-        print(filter_tcp_traffic(raw_packet))
+    for raw_packet in capture:
+        tcp_packet = filter_tcp_traffic(raw_packet)
 
-        print_every(1000, 0)
+        if tcp_packet is not None:
+            tcp_streams = group_tcp_streams_by_ip(tcp_packet, tcp_streams)
+            tcp_streams_statistics = get_tcp_streams_statistics(tcp_packet, tcp_streams, tcp_streams_statistics)
+            #flagged_tcp_streams = flag_tcp_streams(tcp_packet, tcp_streams, tcp_streams_statistics, flagged_tcp_streams)
+
+            print_every(10, tcp_streams_statistics, flagged_tcp_streams)
     
 
 def read_capture_file(file_path):
     tcp_streams = {}
-    tcp_streams_statistics = pandas.DataFrame(columns = ['Stream count', ('Periodicity', 'Spread'), ('Periodicity', 'Jitter'), ('Periodicity', 'Skew'), ('Length', 'Spread'), ('Length', 'Jitter'), ('Length', 'Skew'), 'Malicious'])
+    tcp_streams_statistics = pandas.DataFrame(columns = ['Stream count', ('Periodicity', 'Spread'), ('Periodicity', 'Jitter'), ('Periodicity', 'Skew'), ('Length', 'Spread'), ('Length', 'Jitter'), ('Length', 'Skew'), 'Malicious', 'Flagged'])
     flagged_tcp_streams = []
 
     capture = pyshark.FileCapture(file_path, keep_packets=False, only_summaries=True)
@@ -61,8 +69,37 @@ def read_capture_file(file_path):
         if tcp_packet is not None:
             tcp_streams = group_tcp_streams_by_ip(tcp_packet, tcp_streams)
             tcp_streams_statistics = get_tcp_streams_statistics(tcp_packet, tcp_streams, tcp_streams_statistics)
+            #flagged_tcp_streams = flag_tcp_streams(tcp_packet, tcp_streams, tcp_streams_statistics, flagged_tcp_streams)
 
-            print_every(1000, tcp_streams_statistics)
+            print_every(1000, tcp_streams_statistics, flagged_tcp_streams)
+
+
+def flag_tcp_streams(packet, tcp_streams, tcp_streams_statistics, flagged_tcp_streams):
+    score = 0
+    ip_pair = packet['IP pair']
+    tcp_stream_dataframe = tcp_streams[packet['IP pair']]
+    stream_count = len(tcp_stream_dataframe.index)
+
+    if stream_count > MinStreamCount:
+        if abs(tcp_streams_statistics.at[ip_pair, ('Periodicity', 'Spread')]) < PeriodicitySpreadThreshold:
+            score += 0.25
+
+        if tcp_streams_statistics.at[ip_pair, ('Periodicity', 'Jitter')] < PeriodicityJitterThreshold:
+            score += 0.25
+
+        if abs(tcp_streams_statistics.at[ip_pair, ('Periodicity', 'Skew')]) < SkewLimit:
+            score += 0.25
+
+        if abs(tcp_streams_statistics.at[ip_pair, ('Length', 'Spread')]) < LengthSpreadThreshold:
+            score += 0.25
+
+        if tcp_streams_statistics.at[ip_pair, ('Length', 'Jitter')] < LengthJitterThreshold:
+            score += 0.25
+
+        if score >= 0.75 and ip_pair not in flagged_tcp_streams: flagged_tcp_streams.append(ip_pair)
+        elif ip_pair in flagged_tcp_streams: flagged_tcp_streams.remove(ip_pair)
+
+    return flagged_tcp_streams
 
 def group_tcp_streams_by_ip(packet, tcp_streams):
     tcp_stream = pandas.DataFrame([(packet['Stream index'], packet['Time'], packet['Length'], 0)], columns = ['Stream index', 'Time', 'Length', 'Duration'])
@@ -84,16 +121,33 @@ def group_tcp_streams_by_ip(packet, tcp_streams):
 
     return tcp_streams
 
+def calculate_score(periodicity_spread, periodicity_jitter, periodicity_skew, length_spread, length_jitter):
+    score = 0
 
-def faster_get_tcp_streams_statistics(packet, tcp_streams, tcp_streams_statistics):
-    stream_count = len(tcp_streams[packet['IP pair']].index)
+    if abs(periodicity_spread) < PeriodicitySpreadThreshold:
+        score += 0.25
+
+    if periodicity_jitter < PeriodicityJitterThreshold:
+        score += 0.25
+
+    if abs(periodicity_skew) < SkewLimit:
+        score += 0.25
+
+    if abs(length_spread) < LengthSpreadThreshold:
+        score += 0.25
+
+    if length_jitter < LengthJitterThreshold:
+        score += 0.25
+
+    if score >= 0.75: return 1
+    else: return 0
 
 def get_tcp_streams_statistics(packet, tcp_streams, tcp_streams_statistics):
     tcp_stream_dataframe = tcp_streams[packet['IP pair']]
     stream_count = len(tcp_stream_dataframe.index)
 
     if stream_count > MinStreamCount:
-        time_difference = tcp_stream_dataframe['Time'].diff()
+        time_difference = tcp_stream_dataframe['Time'].diff().iloc[1:] # Removes first row NaN
 
         periodicity_spread = (tcp_stream_dataframe['Time'].sum() / (stream_count * tcp_stream_dataframe['Time'].median())) - 1  # Normalised to the median value (acts as a % of the median as opposed to being massive because of median value)
         periodicity_jitter = (time_difference.std() / time_difference.mean()) * 100
@@ -108,7 +162,9 @@ def get_tcp_streams_statistics(packet, tcp_streams, tcp_streams_statistics):
 
         malicious = 1 if bool(malicious_ip.search(packet['IP pair'])) else 0
 
-        tcp_streams_statistics.loc[packet['IP pair']] = [stream_count, periodicity_spread, periodicity_jitter, periodicity_skew, length_spread, length_jitter, length_skew, malicious]
+        flagged = calculate_score(periodicity_spread, periodicity_jitter, periodicity_skew, length_spread, length_jitter)
+
+        tcp_streams_statistics.loc[packet['IP pair']] = [stream_count, periodicity_spread, periodicity_jitter, periodicity_skew, length_spread, length_jitter, length_skew, malicious, flagged]
 
     return tcp_streams_statistics
 
