f855c6eeefad09f6592841201cb8079b2457692d
Harry Finbow <harryfinbow@gmail.com>
2022-05-09 17:23:17 +0100

    Refactor real-time detection, clean up file structure and update README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # crispy-goggles
+
+**Command and Control (C2) beaconing network testbed and real-time detection system.**
+
+## Building the containers
+
+### Real-time Detector
+
+`docker build -t goggles/detector -f Dockerfile.detector .`
+
+### Command and Control Server
+
+`docker build -t goggles/server -f Dockerfile.server .`
+
+### Benign Traffic Generator
+`docker build -t goggles/benign -f Dockerfile.benign .`
+
+### Compromised Devices
+`docker build -t goggles/compromised -f Dockerfile.compromised .`
+
+## Running the testbed
+To run the testbed with the real-time traffic detector:
+
+`docker compose up --scale tcpdump=0 --scale compromised=10 --scale benign=10`
+
+To run the testbed to generate datasets:
+
+`docker compose up --scale detector=0 --scale compromised=10 --scale benign=10`
+
--- goggles.py
+++ goggles.py
@@ -1,47 +1,41 @@
 import os
-import re
 import time
 import numpy
 import pandas
 import pyshark
-
 from scipy import stats
 
-start_time = time.time()
+pandas.set_option('display.max_rows', 500)
+pandas.set_option('display.max_columns', 500)
+pandas.set_option('display.width', 150)
 
-times_list = []
-packet_list = []
-
-def print_every(rate, scores):
-    if print_every.packets_read % rate == 0:
-        current_time = time.time()
-        statistics = display_statistics(print_every.packets_read, rate, current_time, print_every.last_print)
-
-        print(statistics)
-        print(scores.head(25))
-
-        print_every.last_print = current_time
-    print_every.packets_read += 1
-print_every.packets_read = 0
-print_every.last_print = time.time()
+START_TIME = time.time()
+MINIMUM_STREAM_COUNT = 10
+PACKETS_SINCE_LAST_UPDATE = 1000
 
 
-def display_statistics(packets_read, rate, time, last_print):
-    os.system('cls' if os.name=='nt' else 'clear')
+def filter_packet(packet):
+    if not hasattr(packet, 'tcp'):
+        return
 
-    times_list.append(rate / (time - last_print))
-    packet_list.append(packets_read)
+    return get_packet_details(packet)
 
-    return f'Packets Analysed:   {packets_read}' \
-           f'\nTime Elapsed:       {time - start_time:.2f} seconds' \
-           f'\nPackets per second: {rate / (time - last_print):.0f}'
+    
+def get_packet_details(packet):
+    packet_details = {'Stream index': int(packet.tcp.stream),
+                      'Source':       str(packet.ip.src),
+                      'Destination':  str(packet.ip.dst),
+                      'Time':         float(packet.frame_info.time_relative),
+                      'Length':       int(packet.frame_info.len)}
+    return packet_details
 
 
-def store(packet):
-    source = packet['Source']
+def process_packet(packet):
+    source      = packet['Source']
     destination = packet['Destination']
-    index = packet['Stream index']
+    index       = packet['Stream index']
 
+    # Outbound packets
     if destination in tcp_streams_cache and source in tcp_streams_cache[destination] :
         if index not in tcp_streams_cache[destination][source]:
             tcp_streams_cache[destination][source][index] = [packet['Time'], 0, 0, 0, 0, 0]
@@ -50,7 +44,7 @@ def store(packet):
         tcp_streams_cache[destination][source][index][2] += 1
         tcp_streams_cache[destination][source][index][3] += packet['Length']
 
-    # Source and Destination flipped
+    #  Inbound packets (Source and Destination flipped)
     elif source in tcp_streams_cache and destination in tcp_streams_cache[source]:
         if index not in tcp_streams_cache[source][destination]:
             tcp_streams_cache[source][destination][index] = [packet['Time'], 0, 0, 0, 0, 0]
@@ -59,56 +53,80 @@ def store(packet):
         tcp_streams_cache[source][destination][index][4] += 1
         tcp_streams_cache[source][destination][index][5] += packet['Length']
 
-        return destination, source
+        return {'Source': destination, 'Destination': source}
 
+    # New Destination address
     elif destination not in tcp_streams_cache:
         tcp_streams_cache[destination] = {source: {index: [packet['Time'], 0, 0, 0, 0, 0]}}
 
+    # New Source address
     else:
         tcp_streams_cache[destination][source] = {index: [packet['Time'], 0, 0, 0, 0, 0]}
     
-    return source, destination
+    return {'Source': source, 'Destination': destination}
+
+
+def analyse_packet(packet):
+    source = packet['Source']
+    destination = packet['Destination']
+
+    tcp_streams = tcp_streams_cache[destination][source]
+
+    if len(tcp_streams) < MINIMUM_STREAM_COUNT:
+        return 0
+
+    results = {'Source': source, 'Destination': destination, 'Count': len(tcp_streams),
+               'PeriodicitySpread': 0, 'OutLengthSpread': 0, 'InLengthSpread': 0,
+               'PeriodicityJitter': 0, 'OutLengthJitter': 0, 'InLengthJitter': 0,
+               'PeriodicitySkew'  : 0, 'OutLengthSkew'  : 0, 'InLengthSkew'  : 0,
+                                       'OutLengthMode'  : 0, 'InLengthMode'  : 0}
+
+    values = retrieve_values(tcp_streams)
+    
+    results['PeriodicityJitter'] = (max(values['Periodicity'][1:]) - numpy.mean(values['Periodicity'][1:])) / numpy.mean(values['Periodicity'][1:])
+    results['OutLengthJitter']   = (max(values['Outbound Length']) - numpy.mean(values['Outbound Length'])) / numpy.mean(values['Outbound Length']) 
+    results['InLengthJitter']    = (max(values['Inbound Length']) - numpy.mean(values['Inbound Length'])) / numpy.mean(values['Inbound Length'])
+
+    # Check IP pair within maximum / minimum jitter bounds
+    if results['PeriodicityJitter'] > 10  or results['PeriodicityJitter'] < 0.1 or results['OutLengthJitter'] > 5: 
+        tcp_streams_cache[destination].pop(source)
+        return 0
+
+
+    results['PeriodicitySpread'] = stats.median_abs_deviation(values['Periodicity'][1:])
+    results['OutLengthSpread']   = stats.median_abs_deviation(values['Outbound Length'][:-1])
+    results['InLengthSpread']    = stats.median_abs_deviation(values['Inbound Length'][:-1])
+
+    results['PeriodicitySkew'] = bowleySkew(values['Periodicity'][1:])
+    results['OutLengthSkew']   = bowleySkew(values['Outbound Length'][:-1])
+    results['InLengthSkew']    = bowleySkew(values['Inbound Length'][:-1])
+
+    results['OutLengthMode']     = stats.mode(values['Outbound Length'])[0][0]
+    results['InLengthMode']      = stats.mode(values['Inbound Length'])[0][0]
+
+    malicious = 1 if destination == '172.18.0.2' else 0
+    score, score_breakdown = calculate_score(results, values['Duration'])
+
+    return [source, destination, len(tcp_streams),
+            results['PeriodicitySpread'], results['PeriodicityJitter'], results['PeriodicitySkew'],
+            results['OutLengthSpread'], results['OutLengthJitter'], results['InLengthSpread'],
+            malicious, score, score_breakdown]
 
 
 def retrieve_values(tcp_streams):
     values = { 'Outbound Length': [], 'Inbound Length': [], 'Periodicity': [], 'Duration': 0 }
 
-    # TEMP REPLACEMENT
-    window = -3
-
     start_times = []
-    for stream in list(tcp_streams.keys()):#[-window:]:
+    for stream in list(tcp_streams.keys()):
 
         start_times.append(tcp_streams[stream][0])
         values['Outbound Length'].append(tcp_streams[stream][3])
         values['Inbound Length'].append(tcp_streams[stream][5])
-        #values['Outbound Packets'].append(tcp_streams[stream][2])
-        #values['Inbound Packets'].append(tcp_streams[stream][4])
 
     values['Periodicity'] = numpy.diff(start_times)
     values['Duration'] = start_times[-1] - start_times[0]
 
     return values
-
-
-def calculate_score(results, duration):
-    timeSpreadScore = max(1 - (results['PeriodicitySpread'] / 30), 0) # Seconds
-    outLengthSpreadScore = max(1 - (results['OutLengthSpread'] / 64), 0) # Bytes
-    inLengthSpreadScore = max(1 - (results['InLengthSpread'] / 64), 0) # Bytes
-
-    timeSkewScore = max(1 - abs(results['PeriodicitySkew']), 0) # Seconds
-    outLengthSkewScore = max(1 - abs(results['OutLengthSkew']), 0) # Bytes
-    inLengthSkewScore = max(1 - abs(results['InLengthSkew']), 0) # Bytes
-
-    timeCountScore = min(results['Count'] / (duration / 60), 1) # DIVIDE STREAM COUNT BY NUMBER OF IPs How many there were vs how many there should have been given the time (e.g. 5 / (300/60) = 1)
-    outLengthCharacteristicScore = max(1 - results['OutLengthMode'] / 65535, 0) # Bytes
-    inLengthCharacteristicScore = max(1 - results['InLengthMode'] / 65535, 0) # Bytes
-
-    scores = [timeSpreadScore, outLengthSpreadScore, inLengthSpreadScore, timeSkewScore, timeCountScore, outLengthCharacteristicScore, inLengthCharacteristicScore]
-    score = sum(scores) / len(scores)
-    score_breakdown = " + ".join(map('{:.2f}'.format, scores))
-
-    return score, score_breakdown
 
 
 def bowleySkew(attribute):
@@ -119,105 +137,75 @@ def bowleySkew(attribute):
     mid =  attribute[round(0.50*len(attribute)) - 1]
 
     if high - low != 0 and mid != low and mid != high:
-        return  (low + high - 2*mid) / (mid - low)
+        return (low + high - 2*mid) / (mid - low)
 
     else:
         return 0
 
 
-def analyse(source, destination, tcp_streams_cache):
-    tcp_streams = tcp_streams_cache[destination][source]
+def calculate_score(results, duration):
+    timeSpreadScore = max(1 - (results['PeriodicitySpread'] / 30), 0) # Seconds
+    outLengthSpreadScore = max(1 - (results['OutLengthSpread'] / 64), 0) # Bytes
+    inLengthSpreadScore = max(1 - (results['InLengthSpread'] / 64), 0) # Bytes
 
-    results = {'Source': source, 'Destination': destination, 'Count': len(tcp_streams),
-               'PeriodicitySpread': 0, 'OutLengthSpread': 0, 'InLengthSpread': 0,
-               'PeriodicityJitter': 0, 'OutLengthJitter': 0, 'InLengthJitter': 0,
-               'PeriodicitySkew': 0, 'OutLengthSkew': 0, 'InLengthSkew': 0,
-                                       'OutLengthMode':   0, 'InLengthMode':   0}
+    timeSkewScore = max(1 - abs(results['PeriodicitySkew']), 0) # Seconds
 
-    values = retrieve_values(tcp_streams)
+    timeCountScore = min(results['Count'] / (duration / 60), 1) # DIVIDE STREAM COUNT BY NUMBER OF IPs How many there were vs how many there should have been given the time (e.g. 5 / (300/60) = 1)
+    outLengthCharacteristicScore = max(1 - results['OutLengthMode'] / 65535, 0) # Bytes
+    inLengthCharacteristicScore = max(1 - results['InLengthMode'] / 65535, 0) # Bytes
 
-    results['PeriodicityJitter'] = (max(values['Periodicity'][1:]) - numpy.mean(values['Periodicity'][1:])) / numpy.mean(values['Periodicity'][1:])
-    results['OutLengthJitter']   = (max(values['Outbound Length']) - numpy.mean(values['Outbound Length'])) / numpy.mean(values['Outbound Length']) 
-    results['InLengthJitter']    = (max(values['Inbound Length']) - numpy.mean(values['Inbound Length'])) / numpy.mean(values['Inbound Length'])
+    scores = [timeSpreadScore, timeSkewScore, outLengthSpreadScore, inLengthSpreadScore, timeCountScore, outLengthCharacteristicScore, inLengthCharacteristicScore]
+    score = sum(scores) / len(scores)
+    score_breakdown = " + ".join(map('{:.2f}'.format, scores))
 
-    # Temporary
-   
-    if results['PeriodicityJitter'] > 1  or results['PeriodicityJitter'] < 0.1 or results['OutLengthJitter'] > 1: 
-        tcp_streams_cache[destination].pop(source)
-        return 0
+    return score, score_breakdown
 
 
-    results['PeriodicitySpread'] = stats.median_abs_deviation(values['Periodicity'][1:])
-    results['OutLengthSpread']   = stats.median_abs_deviation(values['Outbound Length'][:-1])
-    results['InLengthSpread']    = stats.median_abs_deviation(values['Inbound Length'][:-1])
+def print_every(rate, scores):
+    if print_every.packets_read % rate == 0:
+        current_time = time.time()
+        statistics = display_statistics(print_every.packets_read, rate, current_time, print_every.last_print)
 
-    '''
-    results['PeriodicitySkew']   = stats.skew(values['Periodicity'][1:])
-    results['OutLengthSkew']     = stats.skew(values['Outbound Length'][:-1])
-    results['InLengthSkew']      = stats.skew(values['Inbound Length'][:-1])
+        print(statistics)
 
-    '''
-
-    results['PeriodicitySkew'] = bowleySkew(values['Periodicity'][1:])
-    results['OutLengthSkew']   = bowleySkew(values['Outbound Length'][:-1])
-    results['InLengthSkew']    = bowleySkew(values['Inbound Length'][:-1])
-
-    results['OutLengthMode']     = stats.mode(values['Outbound Length'])[0][0]
-    results['InLengthMode']      = stats.mode(values['Inbound Length'])[0][0]
-
-    score, score_breakdown = calculate_score(results, values['Duration'])
-
-    return [source, destination, len(tcp_streams),
-            results['PeriodicitySpread'], results['PeriodicityJitter'], results['PeriodicitySkew'],
-            results['OutLengthSpread'], results['OutLengthJitter'], results['OutLengthSkew'],
-            results['InLengthSpread'], results['InLengthJitter'], results['InLengthSkew'],
-            score, score_breakdown]
-    
-
-def get_packet_details(packet):
-    stream_index = int(getattr(packet, 'stream index'))
-    source_address = packet.source
-    destination_address = packet.destination
-    packet_time = float(packet.time)
-    packet_length = int(packet.length)
-    
-    return {'Stream index': stream_index,
-            'Source':       source_address,
-            'Destination':  destination_address,
-            'Time':         packet_time,
-            'Length':       packet_length}
-
-print('Starting....')
-
-tcp_streams_cache = {}
-scores = pandas.DataFrame(columns = ['Stream count', 'pSpread', 'pJitter',  'pSkew', 'oSpread', 'oJitter', 'oSkew', 'iSpread', 'iJitter', 'iSkew', 'Score', 'Score breakdown'])
-
-capture = pyshark.FileCapture('final_report/traffic_data/25_compromised.pcap', keep_packets=False, only_summaries=True)
-#capture = pyshark.LiveCapture(interface='botnet')
-
-results = []
-for packet in capture:
-    print(packet)
-    if hasattr(packet, 'protocol'):
-        if packet.protocol == 'TCP' or packet.protocol.startswith('TLS'):
-            if not packet.info.startswith('[TCP'):
-                if not (packet.source.startswith('192') or packet.destination.startswith('192')):
-                    print(packet.length)
-                    tcp_packet = get_packet_details(packet)
-                    source, destination = store(tcp_packet)
-
-                    if len(tcp_streams_cache[destination][source]) > 3: #and len(tcp_streams_cache[destination][source]) > 0:
-                        results = analyse(source, destination, tcp_streams_cache)
-                        if results: # Temporary
-                            scores.loc[str(results[0]) + '/' + str(results[1])] = results[2:]
-                            scores.sort_values(by='Score', ascending=False, inplace=True)
+        if scores.empty:
+            print("Not enough streams")
+        else:
+            print(scores[['Stream count', 'pSpread', 'pJitter',  'pSkew', 'oSpread', 'oJitter', 'iSpread', 'Malicious', 'Score']].head(25))
         
-            
-    print_every(1000, scores)
 
-print_every(1, scores)
+        print_every.last_print = current_time
+    print_every.packets_read += 1
+print_every.packets_read = 0
+print_every.last_print = time.time()
 
-print(times_list)
-print(packet_list)
 
-### Note: Remove window and just start the container after 1 minute
+def display_statistics(packets_read, rate, time, last_print):
+    os.system('cls' if os.name=='nt' else 'clear')
+
+    return f'Packets Analysed:   {packets_read}' \
+           f'\nTime Elapsed:       {time - START_TIME:.2f} seconds' \
+           f'\nPackets per second: {rate / (time - last_print):.0f}'
+
+
+# Global storage for grouped streams and scores table
+tcp_streams_cache = {}
+scores = pandas.DataFrame(columns = ['Stream count', 'pSpread', 'pJitter',  'pSkew', 'oSpread', 'oJitter', 'iSpread', 'Malicious', 'Score', 'Score breakdown'])
+
+if __name__ == '__main__':
+    capture = pyshark.LiveCapture(interface='botnet', output_file="/tcpdump/tcpdump")
+    #capture = pyshark.FileCapture('tcpdump/tcpdump.pcap')
+
+    for packet in capture:
+        filtered_packet  = filter_packet(packet)
+        if filtered_packet:
+            processed_packet = process_packet(filtered_packet)
+            analysed_packet  = analyse_packet(processed_packet)
+
+            if analysed_packet:
+                scores.loc[str(analysed_packet[0]) + '/' + str(analysed_packet[1])] = analysed_packet[2:]
+                scores.sort_values(by='Score', ascending=False, inplace=True)
+
+            print_every(PACKETS_SINCE_LAST_UPDATE, scores)
+
+    print_every(1, scores)
